fdfe95673d01384eba03c7c9957bb044103489d9
CrossTime <526725723@qq.com>
2023-02-06 10:17:34 +0800

    Update assets.py

--- assets.py
+++ assets.py
@@ -1,1 +1,17 @@
-a="10.10.10.10 test.qihoo.net"
+import os
+import requests
+
+
+a="10.10.10.10 hr.geelib.qihoo.net"
+with open("/etc/hosts", "a+") as f:
+  f.write(a+"\n")
+  
+url = "https://hr.geelib.qihoo.net"
+headers ={"Token": "asdfasDSAFaasdf!@#sadf"}
+username = "hr-admin"
+password = "testMima"
+res = requests.post(url, headers = headers, verify = False, json = {"username": username, "password": password})
+if res.status_code == 301:
+  print("success")
+else:
+  print("failed")
